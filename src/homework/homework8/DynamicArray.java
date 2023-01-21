88d368a1dea1bdcc90d049d23612fa4a9ffb431e
Khachatur Harutyunyan <xchoharutyunyan27@gmail.com>
2023-01-21 22:43:15 +0400

    homework8

---
+++ src/homework/homework8/DynamicArray.java
@@ -0,0 +1,36 @@
+package homework.homework8;
+
+public class DynamicArray {
+    private int[] array = new int[10];
+    private int size;
+
+    DynamicArray() {
+        size = 0;
+    }
+
+    public void add(int value) {
+        if (size == array.length) {
+            extend();
+        }
+        array[size++] = value;
+    }
+
+    private void extend() {
+        int[] tmp = new int[array.length + 10];
+        for (int i = 0; i < size; i++) {
+            tmp[i] = array[i];
+        }
+        array = tmp;
+    }
+
+    public int getByIndex(int index) {
+        return array[index];
+    }
+
+    public void print() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+}
---
+++ src/homework/homework8/DynamicArrayDemo.java
@@ -0,0 +1,24 @@
+package homework.homework8;
+
+public class DynamicArrayDemo {
+    public static void main(String[] args) {
+        DynamicArray myArray = new DynamicArray();
+        myArray.add(27);
+        myArray.add(34);
+        myArray.add(67);
+        myArray.add(21);
+        myArray.add(54);
+        myArray.add(37);
+        myArray.add(58);
+        myArray.add(90);
+        myArray.add(41);
+        myArray.add(22);
+        myArray.add(1);
+        myArray.add(2);
+        myArray.add(3);
+        myArray.add(4);
+        myArray.add(5);
+        myArray.print();
+
+    }
+}
